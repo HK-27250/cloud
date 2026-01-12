5bea640b51575db31fc133eb364d139ae44bc8d6
Hitesh Kumar <127968145+HK-27250@users.noreply.github.com>
2026-01-12 10:06:15 +0100

    Change application title to 'My Application'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Hitesh's application</h1>
+      <h1>My Application</h1>
 
       <input value={title} onChange={e => setTitle(e.target.value)} />
       <button onClick={handleAdd}>Add</button>
