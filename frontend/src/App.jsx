2531ce9d577217f8276a284c82f66c84331b8d43
Hitesh Kumar <127968145+HK-27250@users.noreply.github.com>
2026-01-12 03:21:16 +0100

    Change title from 'Todo App' to 'Todo Apps'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Todo App</h1>
+      <h1>Todo Apps</h1>
 
       <input value={title} onChange={e => setTitle(e.target.value)} />
       <button onClick={handleAdd}>Add</button>
